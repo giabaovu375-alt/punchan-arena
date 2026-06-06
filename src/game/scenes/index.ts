49ba71167479ab02a87e8172ed41452aa8e523a3
baobei-dev <giabaovu375@gmail.com>
2026-06-06 15:58:03 +0700

    Update index.ts

--- src/game/scenes/index.ts
+++ src/game/scenes/index.ts
@@ -1,10 +1,6 @@
-/**
- * Scenes Index - Export tất cả scenes
- */
-
-export { BaseScene } from './BaseScene';
-export { HubScene } from './HubScene';
-export { MainRoadScene } from './MainRoadScene';
-export { LeftForestScene } from './LeftForestScene';
-export { RightPlatformScene } from './RightPlatformScene';
-export { BossScene } from './BossScene';
+export { BaseScene } from "./BaseScene";
+export { HubScene } from "./HubScene";
+// export { MainRoadScene } from "./MainRoadScene"; // khi nào có thì bỏ comment
+// export { LeftForestScene } from "./LeftForestScene";
+// export { RightPlatformScene } from "./RightPlatformScene";
+// export { BossScene } from "./BossScene";
