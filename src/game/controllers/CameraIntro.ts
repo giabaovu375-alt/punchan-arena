46e8a502ee28a2ae79409c3070d15959d6001fe1
baobei-dev <giabaovu375@gmail.com>
2026-06-08 00:15:39 +0700

    Update CameraIntro.ts

--- src/game/controllers/CameraIntro.ts
+++ src/game/controllers/CameraIntro.ts
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { NPC_POSITION, PLAYER_SPAWN } from "./IntroScene";
+import { NPC_POSITION, PLAYER_SPAWN } from "../IntroScene";
 
 // ─────────────────────────────────────────────────────────────────────────────
 // Types
