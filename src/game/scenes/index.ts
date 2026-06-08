4080955e4d5c065133c7c66fcc40aeee89ee1294
baobei-dev <giabaovu375@gmail.com>
2026-06-08 22:49:40 +0700

    Update index.ts

--- src/game/scenes/index.ts
+++ src/game/scenes/index.ts
@@ -1,6 +1,6 @@
 export { BaseScene } from "./BaseScene";
 export { HubScene } from "./HubScene";
-// export { MainRoadScene } from "./MainRoadScene"; // khi nào có thì bỏ comment
-// export { LeftForestScene } from "./LeftForestScene";
-// export { RightPlatformScene } from "./RightPlatformScene";
-// export { BossScene } from "./BossScene";
+export { MainRoadScene } from "./MainRoadScene"; // khi nào có thì bỏ comment
+export { LeftForestScene } from "./LeftForestScene";
+export { RightPlatformScene } from "./RightPlatformScene";
+export { BossScene } from "./BossScene";
